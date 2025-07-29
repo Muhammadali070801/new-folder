fc358fefccbbe70994c88957a3ff9ac3d8f5b05d
Muhammadali070801 <hdsfhjgchfdtyhvb@gmail.com>
2025-07-29 18:29:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// alert('Hello World')
+
+
+// alert obohalantiruvchi boladi 
+// consol F12 bosganida consol bolimda paydo boladigan soz
+
+// let - bu zamaonaviy ozganruvhi doyimo ozgarib turadi 
+// var - bu let ishlab chiqmasdan oldin let oringa ishlab turgan kod 
+// Const - bu ozgarmas birdona kodan boshqasini olmaydi 
+
+       // let        a   =  12;
+//  ozgaruvchi  nomi    qiymati
+
+
+
+// let age = "12"
+// let name = "Ali"
+
+//  console.log(name)
+//  console.log(age)
+
+// alert(name);
+// alert(age);
+
+// var a = 12;
+// var a = 11;
+// var a =3;
+// var b =21;
+// var a = b ;
+// alert(a);
+
+// const a = 22;
+// // const a = 21;
+// const b = 23;
+// alert(a);
+
+
+// "Use Stirick" esi kodlarini uygotib beradi 
